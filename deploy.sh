16fda2387d2b5ac243a2164410e4828299eac6e6
Miguel <jemiguelrivas@gmail.com>
2022-02-05 03:07:34 -0500

    fixing deployment and git router

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env sh
+rm -r -f -v public/**.html
+
+routes=("buttons" "car" "colors" "css" "cube" "drakkar" "gallery" "gear" "grid" "house" "images" "index" "list" "locations" "minivan" "pills" "projects" "spirit" "stargaze" "statistics" "threejs" "videos" "wheel" "window" "xwing")
+
+for f in ${routes[@]}; do
+  echo "
+<meta http-equiv='refresh' content='0; URL=https://miguel-rivas.github.io/2022-vue/'>
+<link rel='canonical' href='https://miguel-rivas.github.io/2022-vue/'>" > public/${f}.html
+done
+
 echo "Getting ready for deployment"
 git push git@github.com:miguel-rivas/2022-vue.git --delete gh-pages
 npm run build
